47b272386b783f3530017fa0d568b38cd5932995
Colin Sasser <p23csasser@fairfieldprep.org>
2022-11-07 12:01:05 -0500

    SUBMISSION

---
+++ lab_6-3.py
@@ -0,0 +1,13 @@
+# Creator CS 11/7/2022
+
+colors = ['red', 'blue', 'yellow', 'green'] #make a list of four colors
+
+colors.extend(['purple', 'black', 'white']) #use colors.extend to add 3 colors
+
+colors.append('Teal') # use colors.append to add another color
+
+colors.insert(3, "Pink") #use insert to add pink at index 3 
+
+colors2 = colors #make a copy
+
+colors.remove('blue') #remove index 2(BLUE)
