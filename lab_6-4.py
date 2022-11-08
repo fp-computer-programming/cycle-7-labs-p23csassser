5b89531d358617bb96525fb613c0988110cbfdae
Colin Sasser <p23csasser@fairfieldprep.org>
2022-11-08 13:19:59 -0500

    submission

---
+++ lab_6-4.py
@@ -0,0 +1,13 @@
+# Creator CS 11/8/2022
+
+subjects = ['APUSH', 'Chem', 'Calc'] #create list of classes
+
+subjects.append('theology') # use subjects.append to add theology 
+
+print(subjects.index('APUSH')) #Find the value of APUSH
+
+subjects.sort() # Sort all the subjects alphabetically 
+
+subjects2 = subjects.copy() #Make a copy of the list 
+
+subjects.sort(reverse=True) #Sort the second list alphatetically
